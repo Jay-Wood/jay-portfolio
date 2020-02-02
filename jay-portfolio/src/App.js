45e09516e3b0c543523befc00b4003ae4a62c9dd
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2020-02-01 16:24:15 -0800

    background image added

--- jay-portfolio/src/App.js
+++ jay-portfolio/src/App.js
@@ -1,42 +1,46 @@
 import React from 'react';
 import './App.css';
-import TopNav from "./TopNav"
+import TopNav from "./TopNav";
+import Background from "./assets/Cloud-background.jpg";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <div 
+      className="App" 
+      // styles={{ backgroundImage:`url(${Background})` }}
+    >        
       <header className="App-header">
         <TopNav />
-
-
-        {/* <nav>
-          <a>about me</a>
-          <a>my work</a>
-        </nav> */}
       </header>
+      <div>
+        <p>My name is Jay and I like to build things.</p>
+      </div>
+      <div>
+        <h2>Projects I've worked on</h2>
         <div>
-          <p>add hero image here</p>
-          <p>My name is Jay and I like to build things.</p>
+          Fish Friends!
         </div>
         <div>
-          <h2>Projects I've worked on</h2>
-          <div>
-            Fish Friends!
-          </div>
-          <div>
-            Pintereach
-          </div>
-          <div>
-            Essentialism
-          </div>
+          Pintereach
         </div>
-    <div>
-      bottom nav
-      <a>github link</a>
-      <a>linked in link</a>
-    </div>
+        <div>
+          Essentialism
+        </div>
+      </div>
+      <div>
+        bottom nav
+        <a>github link</a>
+        <a>linked in link</a>
+      </div>
     </div>
   );
 }
+
+const ImageBackground = styled.img`
+  width: '100%';
+  height: '100%';
+`;
+
 
 export default App;
